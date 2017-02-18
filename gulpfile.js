82798c6944656e400073102d6e0a0cc971eea382
Jack <1162817095@qq.com>
2017-02-18 23:11:48 +0800

    push gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var jade = require('gulp-jade');
 var livereload = require('gulp-livereload');
-var watch = require('gulp-watch');
 var webserver = require('gulp-webserver');
 var plumber = require('gulp-plumber');
 var webserver = require('gulp-webserver');
@@ -22,14 +21,18 @@ var path = {
 };
 gulp.task('clean', function() {
 	return gulp.src(path.dist)
-	.pipe(plumber())
 	.pipe(clean());
 })
-gulp.task('copy', ['clean'], function() {
+gulp.task('copy', function() {
 	return gulp.src(path.assets)
 	.pipe(plumber())
 	.pipe(gulp.dest('dist/assets'));
 });
+gulp.task('copyJs', function() {
+	return gulp.src(path.js)
+	.pipe(plumber())
+	.pipe(gulp.dest(path.dist + '/js'))
+})
 gulp.task('sass', function() {
 	return gulp.src(path.sass)
 	.pipe(plumber())
@@ -39,14 +42,37 @@ gulp.task('sass', function() {
 gulp.task('jade', function() {
 	return gulp.src(path.indexJade)
 	.pipe(plumber())
-	.pipe(jade())
+	.pipe(jade({
+		pretty: true
+	}))
 	.pipe(gulp.dest(path.dist));
 })
+gulp.task('server', function() {
+	return gulp.src(path.dist)
+	.pipe(webserver({
+		livereload: true,
+		open: true,
+		port: '8080'
+	}))
+})
+gulp.task('watch', function() {
+	//重启相关gulp服务
+	gulp.watch(path.sass, ['sass']);
+	gulp.watch(path.assets, ['copy']);
+	gulp.watch(path.js, ['js']);
+	gulp.watch(path.indexJade, ['jade']);
+	//页面自动刷新
+  	gulp.watch(["dist/js/*.js","dist/css/*.css","dist/index.html"])
+    .on("change", livereload.changed)
+});
 gulp.task('default', function() {
 	sequence(
 		'clean',
 		'copy',
+		'copyJs',
 		'sass',
-		'jade'
+		'jade',
+		'server',
+		'watch'
 	)
 })
